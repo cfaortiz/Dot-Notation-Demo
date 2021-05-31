6626c15e9627122e5eef223b4a051e6e2c2c0839
Cesar <>
2021-05-31 23:19:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let myFavorites = {
+  movie: "Shrek",
+  song: "Bohemian Rhapsody",
+  food: "Lasagna",
+};
+
+console.log(myFavorites.movie); // 'Shrek'
+console.log(myFavorites.song); // 'Bohemian Rhapsody'
+console.log(myFavorites.food); // 'Lasagna'
+//console.log(myFavorites.Keys());
+
+// for(let i =0; i<myFavorites.length; i++){
+//   console.log(myFavorites[i])
+// }
